0dc3c5c62a1fb75c03ebd4f634ba3d45c6b2a6a6
lisu <lisu@pld-linux.org>
2010-12-27 09:01:47 +0000

    - create tar.xz package
    Changed files:
        generate-tarball.sh -> 1.2

--- generate-tarball.sh
+++ generate-tarball.sh
@@ -2,9 +2,8 @@
 
 VERSION=$1
 
-rm -rf calibre
 tar -xvzf calibre-$VERSION.tar.gz
 rm -f calibre/resources/fonts/liberation/*
 rm -f calibre/resources/fonts/prs500/*
 
-tar -cvjf calibre-$VERSION-nofonts.tar.bz2 calibre
+tar -cvJf calibre-$VERSION-nofonts.tar.xz calibre
